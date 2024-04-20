a54ecd46b4d07a2e4212ff05c1eab696dddebbf7
Manuel BCabral <manuelsurkc@gmail.com>
2024-04-20 14:28:49 -0500

    Tested troop targeting added grab interactable components

--- Assets/Scripts/WayPoints.cs
+++ Assets/Scripts/WayPoints.cs
@@ -6,7 +6,7 @@ public class WayPoints : MonoBehaviour
 {
     public static Transform[] waypoints;
 
-    void Wake()
+    void Awake ()
     {
         waypoints = new Transform[transform.childCount];
         for(int i = 0; i < waypoints.Length; i++)
